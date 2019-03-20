dcfd20ea6cf45f1ecbbdbed4f79aefeeb9047d87
apolloclark <apolloclark@gmail.com>
2019-03-19 20:56:09 -0400

    added git

--- scripts/install_basics.sh
+++ scripts/install_basics.sh
@@ -5,7 +5,7 @@ export PYTHON_VERSION="3.7.2"
 if [ -x "$(command -v apt-get)" ]; then
 	apt-get update
 	apt-get upgrade -y
-    apt-get install -y aptitude wget curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
+    apt-get install -y aptitude git wget curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
 
     cd /tmp
     wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
@@ -23,7 +23,7 @@ fi
 
 if [ -x "$(command -v yum)" ]; then
     yum update -y
-    yum install -y yum-utils wget curl make gcc openssl-devel bzip2-devel libffi-devel
+    yum install -y yum-utils git wget curl make gcc openssl-devel bzip2-devel libffi-devel
 
     cd /tmp
     wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
