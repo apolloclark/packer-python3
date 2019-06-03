06b54767dc21a1b1037c21ae661a4326d6528ac9
apolloclark <apolloclark@gmail.com>
2019-06-03 18:03:02 -0400

    updated to 3.7.3

--- build_packer_docker.sh
+++ build_packer_docker.sh
@@ -4,7 +4,7 @@ start=`date +%s`
 # ensure that ENV VARs are set
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
 export PACKAGE=${PACKAGE:=python3}
-export PACKAGE_VERSION=${PACKAGE_VERSION:=3.7.2}
+export PACKAGE_VERSION=${PACKAGE_VERSION:=3.7.3}
 export BASE_IMAGE=${BASE_IMAGE:="ubuntu:16.04"}
 export IMAGE_NAME=${IMAGE_NAME:="ubuntu16.04"}
 
--- build_packer_docker_all.sh
+++ build_packer_docker_all.sh
@@ -3,7 +3,7 @@
 # ensure that ENV VARs are set
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
 export PACKAGE=${PACKAGE:=python3}
-export PACKAGE_VERSION=${PACKAGE_VERSION:=3.7.2}
+export PACKAGE_VERSION=${PACKAGE_VERSION:=3.7.3}
 
 ./build_packer_docker_ubuntu16.04.sh
 ./build_packer_docker_centos7.6.sh
