a024a802525a6c85cee63dc8cd1d2a99aac5e536
apolloclark <apolloclark@gmail.com>
2019-08-13 14:44:12 -0400

    refactored tests

--- spec/Dockerfile_debian9.rb
+++ spec/Dockerfile_debian9.rb
@@ -1,55 +1,22 @@
 # spec/Dockerfile_spec.rb
 
-require "serverspec"
-require "docker"
-
-Docker.validate_version!
+require_relative "spec_helper"
 
 describe "Dockerfile" do
   before(:all) do
-    docker_username = ENV['DOCKER_USERNAME']
-    package_name    = ENV['PACKAGE_NAME']
-    package_version = ENV['PACKAGE_VERSION']
-    image_name      = ENV['IMAGE_NAME']
-
-    # check for package version major usage
-    if package_version.match(/(\d+).x/)
-        package_version = package_version.match(/(\d+).x/)[1]
-    end
-
-    image = Docker::Image.get(
-      "#{docker_username}/#{package_name}:#{package_version}-#{image_name}"
-    )
-
-    # https://github.com/mizzy/specinfra
-    # https://docs.docker.com/engine/api/v1.24/#31-containers
-    # https://github.com/swipely/docker-api
-    # https://serverspec.org/resource_types.html
+    load_docker_image()
     set :os, family: :debian
-    set :backend, :docker
-    set :docker_image, image.id
   end
 
-  def os_version
-    command("cat /etc/*-release").stdout
+  describe "Dockerfile#running" do
+    it "runs the right version of Debian" do
+      expect(os_version).to include("Debian")
+      expect(os_version).to include("GNU/Linux 9")
+    end
+    it "runs as root user" do
+      expect(sys_user).to eql("root")
+    end
   end
-
-  def sys_user
-    command("whoami").stdout.strip
-  end
-
-
-
-  it "runs the right version of Debian" do
-    expect(os_version).to include("Debian")
-    expect(os_version).to include("GNU/Linux 9")
-  end
-
-  it "runs as root user" do
-    expect(sys_user).to eql("root")
-  end
-
-
 
   describe command("python --version") do
     its(:exit_status) { should eq 0 }
--- spec/Dockerfile_rhel8.rb
+++ spec/Dockerfile_rhel8.rb
@@ -1,60 +1,28 @@
 # spec/Dockerfile_spec.rb
 
-require "serverspec"
-require "docker"
-
-Docker.validate_version!
+require_relative "spec_helper"
 
 describe "Dockerfile" do
   before(:all) do
-    docker_username = ENV['DOCKER_USERNAME']
-    package_name    = ENV['PACKAGE_NAME']
-    package_version = ENV['PACKAGE_VERSION']
-    image_name      = ENV['IMAGE_NAME']
+    load_docker_image()
+    set :os, family: :redhat
+  end
 
-    # check for package version major usage
-    if package_version.match(/(\d+).x/)
-        package_version = package_version.match(/(\d+).x/)[1]
+  describe "Dockerfile#running" do
+    it "runs the right version of RHEL" do
+      expect(os_version).to include("Red Hat")
+      expect(os_version).to include("release 8")
     end
-
-    image = Docker::Image.get(
-      "#{docker_username}/#{package_name}:#{package_version}-#{image_name}"
-    )
-
-    # https://github.com/mizzy/specinfra
-    # https://docs.docker.com/engine/api/v1.24/#31-containers
-    # https://github.com/swipely/docker-api
-    # https://serverspec.org/resource_types.html
-    set :os, family: :debian
-    set :backend, :docker
-    set :docker_image, image.id
+    it "runs as root user" do
+      expect(sys_user).to eql("root")
+    end
   end
-
-  def os_version
-    command("cat /etc/*-release").stdout
-  end
-
-  def sys_user
-    command("whoami").stdout.strip
-  end
-
-
-
-  it "runs the right version of RHEL" do
-    expect(os_version).to include("Red Hat")
-    expect(os_version).to include("release 8")
-  end
-
-  it "runs as root user" do
-    expect(sys_user).to eql("root")
-  end
-
-
 
   describe command("python --version") do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain ENV['PACKAGE_VERSION'] }
   end
+
   describe command("pip --version") do
     its(:exit_status) { should eq 0 }
   end
